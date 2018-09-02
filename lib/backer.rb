d14620e0228c1056d6eca5ba9a6588fb72155b93
klbennett <https://github.com/settings/applications>
2018-09-02 19:05:02 +0100

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
 
-attr_accessor :backed_projects
+attr_accessor :backed_projects, :name, :title
 
 # #  When a `Backer` instance is initialized, it should be initialized
 # with a `@backed_projects` variable set to an empty array. Instances of the `Backer`
@@ -8,8 +8,14 @@ attr_accessor :backed_projects
 # so that projects can be added to a backer's list and so that the
 # backer can report on the projects they back.
 
-def initialize
+def initialize(name)
+@name = name
 @backed_projects = []
+end
+
+def back_project(project)
+@backed_projects << project
+project.backers << self
 end
 
 end
